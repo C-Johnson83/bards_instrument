50819d73813dd603d9ad362c470f394215c7fd5b
chris <chrisj21283@gmail.com>
2023-10-05 10:56:10 -0400

    got 5 entries. not in days though

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-let cityName = document.querySelector('#cityInput');
-let button = document.querySelector('#searchButton');
+var cityName = document.querySelector('#cityInput');
+var button = document.querySelector('#searchButton');
 var API = apiKey.key;
 var url = "https://api.openweathermap.org/data/2.5/forecast?q="
 var city = document.querySelector('#currentCity');
-var temp = document.querySelector('#currentTemp');
-var wind = document.querySelector('#currentWind');
-var humidity = document.querySelector('#currentHumidity');
-
+var currentTemp = document.querySelector('#currentTemp');
+var currentwind = document.querySelector('#currentWind');
+var currenthumidity = document.querySelector('#currentHumidity');
+var dateVal;
+var day = document.querySelectorAll('#day');
+var temp = document.querySelectorAll('#temp');
+var wind = document.querySelectorAll('#wind');
+var humidity = document.querySelectorAll('#humid');
 function convert(val) {
     return ((val - 273.15) * 9 / 5 + 32).toFixed(2);
 }
@@ -19,18 +23,29 @@ button.addEventListener("click", function () {
         })
         .then(function (data) {
             console.log(data);
-            var cityVal = data.city.name;
-            var tempVal = data.list[0].main.temp;
-            var windVal = data.list[0].wind.speed;
-            var humidityVal = data.list[0].main.humidity;
-            var dateVal = data.list[0].dt_txt;
+            var currentCity = data.city.name;
+            var currentTemp = data.list[0].main.temp;
+            var currentWind = data.list[0].wind.speed;
+            var currentHumidity = data.list[0].main.humidity;
+             dateVal = data.list[0].dt_txt;
             dateVal = dateVal.slice(0, 10);
 
-            city.textContent = 'Current weather of ' + cityVal + ', ' + dateVal;
-            temp.textContent = `Temperature: ${convert(tempVal) + '°F'}`;
-            wind.textContent = `Wind Speed: ${(windVal * 2.23694).toFixed(2)} mph`;
-            humidity.textContent = `Humidity: ${humidityVal}%`;
-      
+            city.textContent = 'Current weather of ' + currentCity + ', ' + dateVal;
+            currentTemp.textContent = `Temperature: ${convert(currentTemp) + '°F'}`;
+            currentwind.textContent = `Wind Speed: ${(currentWind * 2.23694).toFixed(2)} mph`;
+            currenthumidity.textContent = `Humidity: ${currentHumidity}%`;
+            for (var i = 0; i < day.length; i++) {
+                var tempVal = data.list[i].main.temp;
+                var windVal = data.list[i].wind.speed;
+                var humidityVal = data.list[i].main.humidity;
+                var dateVal = data.list[i].dt_txt;
+                // dateVal = dateVal.slice(0, 10);
+                day[i].textContent = dateVal;
+                // png[i]
+                temp[i].textContent = `Temperature: ${convert(tempVal) + '°F'}`;
+                wind[i].textContent = `Wind Speed: ${(windVal * 2.23694).toFixed(2)} mph`;
+                humidity[i].textContent = `Humidity: ${humidityVal}%`;
 
+            }
         });
 });
