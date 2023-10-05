18197893a511dabb6dde2e4a2fd2009e2255c5d1
chris <chrisj21283@gmail.com>
2023-10-05 13:22:26 -0400

    add j to loop days not hours. tried to get icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,11 @@ var currentwind = document.querySelector('#currentWind');
 var currenthumidity = document.querySelector('#currentHumidity');
 var dateVal;
 var day = document.querySelectorAll('#day');
+var png = document.querySelectorAll('#png');
 var temp = document.querySelectorAll('#temp');
 var wind = document.querySelectorAll('#wind');
 var humidity = document.querySelectorAll('#humid');
+var j = 0;
 function convert(val) {
     return ((val - 273.15) * 9 / 5 + 32).toFixed(2);
 }
@@ -27,7 +29,7 @@ button.addEventListener("click", function () {
             var currTemp = data.list[0].main.temp;
             var currWind = data.list[0].wind.speed;
             var currHumidity = data.list[0].main.humidity;
-             dateVal = data.list[0].dt_txt;
+            dateVal = data.list[0].dt_txt;
             dateVal = dateVal.slice(0, 10);
 
             city.textContent = 'Current weather of ' + currCity + ', ' + dateVal;
@@ -35,17 +37,23 @@ button.addEventListener("click", function () {
             currentwind.textContent = `Wind Speed: ${(currWind * 2.23694).toFixed(2)} mph`;
             currenthumidity.textContent = `Humidity: ${currHumidity}%`;
             for (var i = 0; i < day.length; i++) {
-                var tempVal = data.list[i].main.temp;
-                var windVal = data.list[i].wind.speed;
-                var humidityVal = data.list[i].main.humidity;
-                var dateVal = data.list[i].dt_txt;
-                // dateVal = dateVal.slice(0, 10);
+                console.log('current i', i);
+                console.log('current j', j);
+                console.log(data.list[j].dt_txt);
+                var tempVal = data.list[j].main.temp;
+                var windVal = data.list[j].wind.speed;
+                var humidityVal = data.list[j].main.humidity;
+                var dateVal = data.list[j].dt_txt;
+                var iconVal = data.list[j].weather[0].icon;
+                var icon = 'https://openweathermap.org/img/wn/' + iconVal + '.png';
+
+                dateVal = dateVal.slice(0, 10);
+
                 day[i].textContent = dateVal;
-                // png[i]
+                png[i].setAttribute('src', icon);
                 temp[i].textContent = `Temperature: ${convert(tempVal) + '°F'}`;
                 wind[i].textContent = `Wind Speed: ${(windVal * 2.23694).toFixed(2)} mph`;
                 humidity[i].textContent = `Humidity: ${humidityVal}%`;
-
+                j = j + 8;
             }
-        });
-});
+        });});
