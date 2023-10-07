bb23b80d1736c8585cf612cf89b43fd510ada2fd
chris <chrisj21283@gmail.com>
2023-10-07 18:06:24 -0400

    add button and click event for each searched city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var cityName = $('#cityInput');
 var button = $('#searchButton');
+var citySearched = $('#cityButton');
 var API = 'cebdbe1753a5af12101fc266dce79204';
 var url = "https://api.openweathermap.org/data/2.5/forecast?q="
 var city = $('#currentCity');
 var forecast = $('#spread');
+var searches = $('#searchedCities');
 var currentTemp = $('#currentTemp');
 var currentwind = $('#currentWind');
 var currenthumidity = $('#currentHumidity');
@@ -14,26 +16,44 @@ function convert(val) {
 }
 
 button.on("click", function () {
-    
-    var queryUrl = url + cityName.val() + "&appid=" + API;
+    destination = cityName.val();
+    getWeather(destination);
+   
+});
+
+$(document).on("click", "#cityButton", function (event) {
+    cityName = $(this).text();;
+    console.log(event)
+    console.log(cityName);
+getWeather(cityName);
+});
+    function getWeather(cityName) {
+    var queryUrl = url + cityName + "&appid=" + API;
     fetch(queryUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
             console.log(data);
             forecast.empty();
             var currCity = data.city.name;
             var currTemp = data.list[0].main.temp;
             var currWind = data.list[0].wind.speed;
             var currHumidity = data.list[0].main.humidity;
+            var cityButton = $('<button id="cityButton">');
             dateVal = data.list[0].dt_txt;
             dateVal = dateVal.slice(0, 10);
-            
+
             city.text('Current weather of ' + currCity + ', ' + dateVal);
-            currentTemp.text ( `Temperature: ${convert(currTemp) + '°F'}`);
-            currentwind.text ( `Wind Speed: ${(currWind * 2.23694).toFixed(2)} mph`);
-            currenthumidity.text  (`Humidity: ${currHumidity}%`);
+            currentTemp.text(`Temperature: ${convert(currTemp) + '°F'}`);
+            currentwind.text(`Wind Speed: ${(currWind * 2.23694).toFixed(2)} mph`);
+            currenthumidity.text(`Humidity: ${currHumidity}%`);
+
+            localStorage.setItem('city', cityName.val);
+            searchedCities.push(cityName.val);
+            cityButton.text(currCity);
+            searches.append(cityButton);
+
             for (var i = 0; i < 40; i += 8) {
                 var tempVal = data.list[i].main.temp;
                 var windVal = data.list[i].wind.speed;
@@ -42,26 +62,24 @@ button.on("click", function () {
                 var iconVal = data.list[i].weather[0].icon;
                 var icon = 'https://openweathermap.org/img/wn/' + iconVal + '@2x.png';
                 dateVal = dateVal.slice(0, 10); // removes the time from the date
-                
-                
+
+
                 var card = $('<div class="card"></div>');
                 var day = $('<h4>' + dateVal + '</h4>');
                 var png = $('<img src="' + icon + '">');
                 var temp = $('<p>Temperature: ' + convert(tempVal) + '°F</p>');
                 var wind = $('<p>Wind Speed: ' + (windVal * 2.23694).toFixed(2) + ' mph</p>');
                 var humidity = $('<p>Humidity: ' + humidityVal + '%</p>');
-                
+
                 card.append(day);
                 card.append(png);
                 card.append(temp);
                 card.append(wind);
                 card.append(humidity);
-                
+
                 forecast.append(card);
-                
+
             }
         });
-        localStorage.setItem('city', cityName.val());
-        searchedCities.push(cityName.val());
-        console.log(searchedCities);
-});
+
+};
